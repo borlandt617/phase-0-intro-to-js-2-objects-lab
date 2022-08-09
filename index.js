8dd8d35f9f6fba84683118826da20f501df4ad8d
david borland <borlandt617@gmail.com>
2022-08-09 19:11:00 -0400

    commit tree 07f30744a2c669324bc901085206f8a1d3b2494a

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+const employee = {
+name: "Sam",
+StreetAddress: "11 Broadway",
+
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   employee[key] = value;
+   
+   return employee;
+
+}
+function nondestructivelyUpdateemployee(employee, key, value) {
+    const newemployee = { ...employee };
+    newEmployee[key] = value;
+    return newEmployee;
+
+
+}
+console.log(newEmployee)
